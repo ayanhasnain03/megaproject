e84102019b9a29cd05ac049f545b249025a8faa8
Ayan <ayan2572006@gmail.com>
2023-12-23 02:34:48 +0530

    complete forget and reset password

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { registerUser ,loginUser, logoutUser} = require("../controllers/userController");
+const { registerUser ,loginUser, logoutUser, forgotPassword, resetPassword} = require("../controllers/userController");
 const Router = express.Router();
 
 Router.route("/register").post(registerUser);
 Router.route("/login").post(loginUser);
 Router.route("/logout").post(logoutUser);
-
+Router.route("/password/forgot").post(forgotPassword);
+Router.route("/password/reset/:token").put(resetPassword);
 module.exports = Router;
