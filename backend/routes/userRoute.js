1d1a9846c456bd2e3ae1cc63344b0378c19ad9b1
Ayan <ayan2572006@gmail.com>
2023-12-23 13:57:44 +0530

    complete user authenthication

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,21 @@
 const express = require("express");
-const { registerUser ,loginUser, logoutUser, forgotPassword, resetPassword} = require("../controllers/userController");
+const { isAuthenticatedUser, authorizedRoles } = require("../middlewares/auth");
+const { registerUser, loginUser, logoutUser, forgotPassword, resetPassword, getUserDetails, updatePassword, updateProfile } = require("../controllers/userController");
 const Router = express.Router();
-
+//register
 Router.route("/register").post(registerUser);
+//login
 Router.route("/login").post(loginUser);
+//logout
 Router.route("/logout").post(logoutUser);
+// send token on email
 Router.route("/password/forgot").post(forgotPassword);
+// password reset via email roken
 Router.route("/password/reset/:token").put(resetPassword);
+// get user own details
+Router.route("/me").get(isAuthenticatedUser, getUserDetails);
+// updater user password
+Router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+// update user forfile
+Router.route("/me/update").put(isAuthenticatedUser, updateProfile);
 module.exports = Router;
