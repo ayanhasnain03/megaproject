c2260421b1196761f3ad37df66c52979c554538f
Ayan <ayan2572006@gmail.com>
2023-12-24 17:40:11 +0530

    fix server undefined issue

--- backend/config/conn.js
+++ backend/config/conn.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
     mongoose.connect(process.env.DB_URL).then((data) => {
-        console.log("db connected");
+        console.log("db connected")
+    }).catch((e)=>{
+        console.log("db diconnected")
     })
 }
 
